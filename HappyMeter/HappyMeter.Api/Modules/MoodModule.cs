f495369757db7c0750da3e0c5fd70fb79370e8ee
Peter Qwarnstrom <peter.qwarnstrom@gmail.com>
2014-12-09 00:48:40 +0100

    Nancy model for mood

---
+++ HappyMeter/HappyMeter.Api/Modules/MoodModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HappyMeter.Api.Modules
+{
+	public class MoodModule : Nancy.NancyModule
+	{
+		public MoodModule()
+		{
+			Get["/mood"] = _ => "Peter was here!";
+		}
+	}
+}
