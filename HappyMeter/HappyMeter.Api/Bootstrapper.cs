7a1458fabc2cddb09c09bafdedc482f0e7045962
Peter Qwarnstrom <peter.qwarnstrom@gmail.com>
2014-12-12 23:54:20 +0100

    Added new project for web

--- HappyMeter/HappyMeter.Api/Bootstrapper.cs
+++ HappyMeter/HappyMeter.Api/Bootstrapper.cs
@@ -10,41 +10,41 @@ namespace HappyMeter.Api
 {
 	public class Bootstrapper : Nancy.DefaultNancyBootstrapper
 	{
-			protected override void ConfigureApplicationContainer(Nancy.TinyIoc.TinyIoCContainer container)
-			{
-				base.ConfigureApplicationContainer(container);
-				container.Register<Raven.Client.IDocumentStore>(GenerateRavenDocStore());
-			}
+		protected override void ConfigureApplicationContainer(Nancy.TinyIoc.TinyIoCContainer container)
+		{
+			base.ConfigureApplicationContainer(container);
+			container.Register<Raven.Client.IDocumentStore>(GenerateRavenDocStore());
+		}
 
-			private Raven.Client.IDocumentStore GenerateRavenDocStore()
-			{
-				var ravenUrl = ConfigurationManager.AppSettings["RavenUrl"];
-				var ravenApiKey = ConfigurationManager.AppSettings["RavenApiKey"];
-	
-				var docstore = new Raven.Client.Document.DocumentStore
-				{
-					//ConnectionStringName = "RavenDB"
-					Url = ravenUrl,
-					ApiKey = ravenApiKey
-				};
-				docstore.Initialize();
+		private Raven.Client.IDocumentStore GenerateRavenDocStore()
+		{
+			var ravenUrl = ConfigurationManager.AppSettings["RavenUrl"];
+			var ravenApiKey = ConfigurationManager.AppSettings["RavenApiKey"];
 
-				return docstore;
-			}
-
-			protected override void ConfigureRequestContainer(
-					Nancy.TinyIoc.TinyIoCContainer container, Nancy.NancyContext context)
+			var docstore = new Raven.Client.Document.DocumentStore
 			{
-				base.ConfigureRequestContainer(container, context);
-				container.Register<Raven.Client.IDocumentSession>(GenerateRavenSession(container));
-			}
+				//ConnectionStringName = "RavenDB"
+				Url = ravenUrl,
+				ApiKey = ravenApiKey
+			};
+			docstore.Initialize();
 
-			private Raven.Client.IDocumentSession GenerateRavenSession(
-					Nancy.TinyIoc.TinyIoCContainer container)
-			{
-				var store = container.Resolve<Raven.Client.IDocumentStore>();
-				var session = store.OpenSession();
-				return session;
-			}
+			return docstore;
+		}
+
+		protected override void ConfigureRequestContainer(
+				Nancy.TinyIoc.TinyIoCContainer container, Nancy.NancyContext context)
+		{
+			base.ConfigureRequestContainer(container, context);
+			container.Register<Raven.Client.IDocumentSession>(GenerateRavenSession(container));
+		}
+
+		private Raven.Client.IDocumentSession GenerateRavenSession(
+				Nancy.TinyIoc.TinyIoCContainer container)
+		{
+			var store = container.Resolve<Raven.Client.IDocumentStore>();
+			var session = store.OpenSession();
+			return session;
+		}
 	}
 }
